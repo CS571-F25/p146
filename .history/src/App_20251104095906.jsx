b22de9ab774ba8367f9af01477a58bbdda03a655
Kristen Anderson <kmanderson43@wisc.edu>
2025-11-04 10:06:21 -0600

    first commit

---
+++ .history/src/App_20251104095906.jsx
@@ -0,0 +1,13 @@
+import { HashRouter, Route, Routes } from 'react-router'
+import './App.css'
+
+function App() {
+  return <HashRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<AboutMe/>}></Route>
+    </Routes>
+  </HashRouter>
+}
+
+export default App
